96020f3cf6895fef71bb62a2cabdd5a88b3bf03c
Dannauc <danna.uchimacu@amigo.edu.co>
2024-09-11 17:04:11 -0500

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -1,12 +1,11 @@
-public class Autor {
+public class Autor extends Persona {
     private int autorID;
-    private String nombre;
     private String nacionalidad;
     private String biografia;
 
     public Autor(int autorID, String nombre, String nacionalidad, String biografia) {
+        super(nombre, null); // No usamos email para Autor
         this.autorID = autorID;
-        this.nombre = nombre;
         this.nacionalidad = nacionalidad;
         this.biografia = biografia;
     }
@@ -16,12 +15,29 @@ public class Autor {
     }
 
     public void modificarAutor(String nuevoNombre, String nuevaBiografia) {
-        this.nombre = nuevoNombre;
+        setNombre(nuevoNombre);
         this.biografia = nuevaBiografia;
         System.out.println("Autor modificado: " + nombre);
     }
 
     public void eliminarAutor() {
         System.out.println("Autor eliminado: " + nombre);
+    }
+
+    // Getters y Setters
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
     }
 }
